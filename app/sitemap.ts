b764b0e43b5d7b0affd664f38b97c589a2c5b1f1
DoctorAcid <doctoracid12@gmail.com>
2026-05-28 20:35:05 +0530

    fix: sitemap.xml and robots.txt update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,24 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://madushan.design/",
-      changeFrequency: "monthly",
+      url: 'https://madushan.design',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: "https://madushan.design/work",
-      changeFrequency: "monthly",
+      url: 'https://madushan.design/works',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: "https://madushan.design/contact",
-      changeFrequency: "yearly",
+      url: 'https://madushan.design/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
       priority: 0.8,
     },
-  ];
+  ]
 }
